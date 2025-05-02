a78be413d4d04ae3ab6b7b4a9344112c3c3dbab3
Yehhmii <franciscoabhulimen@gmail.com>
2025-05-01 18:50:11 -0600

    basis of ext js

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+  const response = await fetch('http://localhost:3000/api/books')
+  const data = await response.json()
   return (
-    <div>the about page is slow </div>
+    <>
+    <div>
+      <h2 className='text-2xl'>About </h2>
+       </div>
+      <div>
+      {data.map((book: { id: number; title: string; author: string }) => (
+        <div key={book.id}>
+          <h2>{book.title}</h2>
+          <p>{book.author}</p>
+        </div>
+      ))}
+      </div>
+    
+    </>
+
   )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hello from "@/app/components/hello";
 
 export default function Home() {
+
   return (
     <>
       <h2 className="text-3xl">Welcome to next.js</h2>
